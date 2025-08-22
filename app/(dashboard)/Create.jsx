0d7d29072213e8929ab942c8e75ed8d3e08b23d3
jorgemedinaleigh <jorgemedinaleigh@protonmail.com>
2025-08-22 18:24:54 -0400

    implemented functionality to create books

--- app/(dashboard)/Create.jsx
+++ app/(dashboard)/Create.jsx
@@ -1,16 +1,76 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { useState } from 'react'
+import { useRouter } from 'expo-router'
+import { useBooks } from '../../hooks/useBooks'
 import Spacer from '../../components/Spacer'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
+import ThemedTextInput from '../../components/ThemedTextInput'
+import ThemedButton from '../../components/ThemedButton'
 
 const Create = () => {
+  const [title,setTitle] = useState('')
+  const [author,setAuthor] = useState('')
+  const [description,setDescription] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const { createBook } = useBooks()
+  const router = useRouter()
+
+  const handleSubmit = async () => {
+    if (!title.trim() || !author.trim() || !description.trim()){
+      alert('Please fill in all fields.')
+      return
+    }
+
+    setLoading(true)
+    await createBook({ title, author, description })
+    setTitle('')
+    setAuthor('')
+    setDescription('')
+
+    router.replace('/Books')
+    setLoading(false)
+  }
+
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText title={true} style={styles.heading}>
-        Add a New Book
-      </ThemedText>
-      <Spacer/>
-    </ThemedView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ThemedView style={styles.container}>
+        <ThemedText title={true} style={styles.heading}>Add a New Book</ThemedText>
+        <Spacer />
+
+        <ThemedTextInput 
+          placeholder="Title"
+          style={styles.input}
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Spacer />
+
+        <ThemedTextInput 
+          placeholder="Author"
+          style={styles.input}
+          value={author}
+          onChangeText={setAuthor}
+        />
+        <Spacer />
+
+        <ThemedTextInput 
+          placeholder="Book Description"
+          multiline={true}
+          style={styles.multiline}
+          value={description}
+          onChangeText={setDescription}
+        />
+        <Spacer />
+
+        <ThemedButton onPress={handleSubmit} disabled={loading}>
+          <Text style={{ color: '#fff' }}>
+            {loading ? 'Saving...' : 'Create Book'}
+          </Text>
+        </ThemedButton>
+      </ThemedView>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -26,5 +86,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     textAlign: 'center',
+  },
+  input: {
+    padding: 20,
+    borderRadius: 6,
+    alignSelf: 'stretch',
+    marginHorizontal: 40
+  },
+  multiline: {
+    padding: 20,
+    borderRadius: 6,
+    minHeight: 100,
+    alignSelf: 'stretch',
+    marginHorizontal: 40
   }
 })
--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react";
+import { databases } from '../lib/appwrite'
+import { ID, Permission, Role } from "react-native-appwrite";
+import { useUser } from "../hooks/useUser";
 
 const DATABASE_ID = '68a8e0db0036e57440b1'
 const COLLECTION_ID = '68a8e114002637b99b0d'
@@ -7,6 +10,7 @@ export const BooksContext = createContext()
 
 export function BooksProvider({ children }) {
   const [books, setBooks] = useState([])
+  const { user } = useUser()
 
   async function fetchBooks(){
     try{
@@ -26,7 +30,17 @@ export function BooksProvider({ children }) {
   }
   async function createBook(data){
     try{
-
+      const newBook = await databases.createDocument(
+        DATABASE_ID, 
+        COLLECTION_ID, 
+        ID.unique(), 
+        {...data, userId: user.$id},
+        [
+          Permission.read(Role.user(user.$id)),
+          Permission.update(Role.user(user.$id)),
+          Permission.delete(Role.user(user.$id))
+        ]
+      )
     }
     catch (error){
       console.error(error.message)
