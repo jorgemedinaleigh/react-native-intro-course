df8b6b979699dc80488da488e1ed043a5a408af4
jorgemedinaleigh <jorgemedinaleigh@protonmail.com>
2025-08-22 20:10:27 -0400

    implemented functionality to delete a book

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native'
-import { useLocalSearchParams } from 'expo-router'
+import { StyleSheet, Text } from 'react-native'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import { useEffect, useState } from 'react'
 import { useBooks } from '../../../hooks/useBooks'
 import ThemedView from '../../../components/ThemedView'
@@ -7,11 +7,20 @@ import ThemedText from '../../../components/ThemedText'
 import Spacer from '../../../components/Spacer'
 import ThemedCard from '../../../components/ThemedCard'
 import ThemedSpinner from '../../../components/ThemedSpinner'
+import ThemedButton from '../../../components/ThemedButton'
+import { Colors } from '../../../constants/Colors'
 
 const BookDetails = () => {
   const [book, setBook] = useState(null)
   const { id } = useLocalSearchParams()
-  const { fetchBookById } = useBooks()
+  const { fetchBookById, deleteBook } = useBooks()
+  const router = useRouter()
+
+  const handleDelete = async () => {
+    await deleteBook(id)
+    setBook(null)
+    router.replace('/Books')
+  }
 
   useEffect(() => {
     async function loadBook(){
@@ -41,6 +50,10 @@ const BookDetails = () => {
         <Spacer height={10} />
         <ThemedText>{book.description}</ThemedText>
       </ThemedCard>
+
+      <ThemedButton style={styles.delete} onPress={handleDelete}>
+        <Text style={{ color: '#fff', textAlign: 'center' }}>Delete</Text>
+      </ThemedButton>
     </ThemedView>
   )
 }
@@ -62,5 +75,11 @@ const styles = StyleSheet.create({
     margin: 20,
     padding: 10,
     borderRadius: 6
+  },
+  delete: {
+    marginTop: "40",
+    backgroundColor: Colors.warning,
+    width: "50%",
+    alignSelf: "center"
   }
 })
--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -52,7 +52,7 @@ export function BooksProvider({ children }) {
   }
   async function deleteBook(id) {
    try{
-
+      await databases.deleteDocument(DATABASE_ID, COLLECTION_ID, id)
     }
     catch (error){
       console.error(error.message)
@@ -71,6 +71,9 @@ export function BooksProvider({ children }) {
 
         if (events[0].includes('create')){
           setBooks((prevBooks) => [...prevBooks, payload])
+        }
+        if (events[0].includes('delete')){
+          setBooks((prevBooks) => prevBooks.filter((book) => book.$id !== payload.$id))
         }
       })
     }
